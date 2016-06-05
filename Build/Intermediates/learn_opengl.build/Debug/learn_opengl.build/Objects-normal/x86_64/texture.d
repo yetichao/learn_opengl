1a525b23433f7f701c84535687c88ea2136da6a1
xueqin.chen <cc_xueqin@xueqinchendeMacBook-Air.local>
2016-06-05 11:14:57 +0800

    rm Build

--- Build/Intermediates/learn_opengl.build/Debug/learn_opengl.build/Objects-normal/x86_64/texture.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/cc_xueqin/programming/learning/opengl/project/learn_opengl/learn_opengl/texture.cpp \
-  /Users/cc_xueqin/programming/learning/opengl/project/learn_opengl/learn_opengl/texture.h \
-  /opt/local/include/GL/glew.h /opt/local/include/GLFW/glfw3.h \
-  /opt/local/include/SOIL/SOIL.h \
-  /Users/cc_xueqin/programming/learning/opengl/project/learn_opengl/learn_opengl/shader_str.h \
-  /Users/cc_xueqin/programming/learning/opengl/project/learn_opengl/learn_opengl/utill.h
--- Build/Intermediates/learn_opengl.build/Debug/learn_opengl.build/Objects-normal/x86_64/utill.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/cc_xueqin/programming/learning/opengl/project/learn_opengl/learn_opengl/utill.cpp \
-  /Users/cc_xueqin/programming/learning/opengl/project/learn_opengl/learn_opengl/utill.h \
-  /opt/local/include/GLFW/glfw3.h
